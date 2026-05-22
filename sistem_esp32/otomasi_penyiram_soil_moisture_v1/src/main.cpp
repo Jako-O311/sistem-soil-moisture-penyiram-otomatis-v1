6c0d5f0488e6664c4380b1da2d84d555d37b8226
Jati Prakoso <jatip0311@gmail.com>
2026-05-22 22:07:56 +0700

    kembali ke awal dan menambah sensor DHT22
    merombak sistem dari awal, dan penambahan sensor kelembapan udara dht22

--- sistem_esp32/otomasi_penyiram_soil_moisture_v1/src/main.cpp
+++ sistem_esp32/otomasi_penyiram_soil_moisture_v1/src/main.cpp
@@ -3,13 +3,17 @@
 #include <LiquidCrystal_I2C.h>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <DHTesp.h>
 
 // // put function declarations here:
 // int myFunction(int, int);
 
 // pinout
 const int soilMoisturePin1 = 34;  // Pin untuk sensor kelembaban tanah 1
-const int soilMoisturePin2 = 35;  // Pin untuk sensor kelembaban tanah 2
+// const int soilMoisturePin2 = 35;  // Pin untuk sensor kelembaban tanah 2
+// DHTesp dht; // Inisialisasi objek DHT
+// const int dhtPin = 17; // Pin untuk sensor kelembaban udara DHT22
+// TempAndHumidity dhtData; // Variabel untuk menyimpan data sensor DHT22
 const int relayPin = 12;          // Pin untuk relay 1 channel
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 // threshold kelembaban dalam persen
@@ -28,13 +32,16 @@ void setup() {
   // Set pin relay sebagai output
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, HIGH);
+  // // setup sensor DHT22
+  // dht.setup(dhtPin, DHTesp::DHT22);
+  // dhtData = dht.getTempAndHumidity(); // Baca data awal dari DHT22
   // setup wifi
-  WiFi.begin(ssid, password);
-  Serial.println("koneksi ke wifi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+  // WiFi.begin(ssid, password);
+  // Serial.println("koneksi ke wifi");
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
   // server.on("/", []() {
   //   server.send(200, "text/plain", "test123");
   // });
@@ -45,9 +52,9 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Soil Moisture");
+  lcd.print("soil moisture");
   lcd.setCursor(0, 1);
-  lcd.print("System Ready");
+  lcd.print("sistem siap");
   delay(2000);
   lcd.clear();
 }
@@ -56,16 +63,19 @@ void loop() {
   // put your main code here, to run repeatedly:
   // baca nilai analog dari sensor (0-4095 untuk ESP32)
   int sensorValue1 = analogRead(soilMoisturePin1);
-  int sensorValue2 = analogRead(soilMoisturePin2);
+  // int sensorValue2 = analogRead(soilMoisturePin2);
   // konversi ke persentase kelembaban
-  // asumsikan 0 = basah 100%, 4095 = kering 0%)
+  // diasumsikan jika 0 = basah 100%, 4095 = kering 0%)
   // sesuaikan mapping berdasarkan kalibrasi sensor
-  int moisturePercent1 = map(sensorValue1, 0, 4095, 100, 0) + 10;
-  int moisturePercent2 = map(sensorValue2, 0, 4095, 100, 0) + 10;
+  int moisturePercent1 = map(sensorValue1, 0, 4095, 100, 0);
+  // int moisturePercent2 = map(sensorValue2, 0, 4095, 100, 0) + 10;
   // hitung rata kelembaban
-  int avgMoisture = (moisturePercent1 + moisturePercent2) / 2;
+  // int avgMoisture = (moisturePercent1 + moisturePercent2) / 2;
   // kontrol relay - relay menyala jika rata-rata kelembaban di bawah threshold
-  if (avgMoisture < moistureThreshold) {
+  // delay(dht.getMinimumSamplingPeriod()); // Tunggu sesuai dengan periode sampling minimum DHT22
+  // dht.getTempAndHumidity(); // Update data DHT22
+
+  if (moisturePercent1 < moistureThreshold) {
     digitalWrite(relayPin, LOW);  // Nyalakan relay untuk penyiraman
   } else {
     digitalWrite(relayPin, HIGH); // Matikan relay
@@ -74,9 +84,9 @@ void loop() {
   lcd.setCursor(0, 0);
   lcd.print("S1:");
   lcd.print(moisturePercent1);
-  lcd.print("% S2:");
-  lcd.print(moisturePercent2);
   lcd.print("%");
+  // lcd.print(moisturePercent2);
+  // lcd.print("%");
   lcd.setCursor(0, 1);
   lcd.print("Relay: ");
   if (digitalRead(relayPin) == LOW) {
@@ -87,11 +97,12 @@ void loop() {
   // tampilan untuk serial monitor untuk debugging
   Serial.print("Sensor1: ");
   Serial.print(moisturePercent1);
-  Serial.print("%, Sensor2: ");
-  Serial.print(moisturePercent2);
-  Serial.print("%, Avg: ");
-  Serial.print(avgMoisture);
-  Serial.print("%, Relay: ");
+  // Serial.print("%, Sensor2: ");
+  // Serial.print(moisturePercent2);
+  // Serial.print("%, Avg: ");
+  // Serial.print(avgMoisture);
+  // Serial.println("Temp: "+String(dhtData.temperature, 2)+"'C Kelembaban: "+String(dhtData.humidity, 1)+"%");
+  Serial.print("Relay: ");
   Serial.println(digitalRead(relayPin) == LOW ? "ON" : "OFF");
   // delay 1 detik
   delay(1000);
